698d06db47b6de9dc90571d69e8c7745e2f5ad4d
unknown <rajarao.seelam@cgi.com>
2019-09-04 12:43:22 +0530

    testing-assignment

---
+++ isogram-csharp-problem/Isogram.Tests/Test_Isogram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using isogram_csharp_problem;
+
+namespace Isogram.Tests
+{
+    public class Test_Isogram
+    {
+        [Theory]
+        [InlineData("", true)]
+        [InlineData("isogram", true)]
+        [InlineData("eleven", false)]
+        [InlineData("Alphabet", true)]
+        [InlineData("subdermatoglyphic", true)]
+        [InlineData("thumbscrew-japingly", true)]
+        [InlineData("six-year-old", true)]
+        [InlineData("raj s", true)]
+
+        public void Theory_Check(string value, bool expected)
+        {
+            Assert.Equal(expected, isogram.check(value));
+        }
+
+    }
+}
---
+++ isogram-csharp-problem/isogram-csharp-problem/isogram.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace isogram_csharp_problem
+{
+    public class isogram
+    {
+
+        public static bool check(string s)
+        {
+            HashSet<char> iso = new HashSet<char>();
+            bool flag = true;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '-' || s[i] == ' ') continue;
+
+                else
+                {
+
+                    if (iso.Contains(s[i]))
+                    {
+                        flag = false;
+                        break;
+                    }
+
+                    else
+                    {
+                        iso.Add(s[i]);
+                    }
+                }
+ 
+            }
+            return flag;
+        }
+
+
+
+            public static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+
+
